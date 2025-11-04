3562ed6328f7edf07266ab79ad9acac581cb4906
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-11-04 14:37:15 +0100

    fix ESLint alerts

--- theme_changer.js
+++ theme_changer.js
@@ -1,22 +1,22 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-  const themeButton = document.getElementById('themeButton');
-  const themeButtonSpan = themeButton.querySelector('span');
-  
+  const
+    themeButton = document.querySelector('#themeButton'),
+    themeButtonSpan = themeButton.querySelector('span');
+
   let currentThemeSetting = localStorage.getItem('theme');
-  themeButtonSpan.innerText = `${currentThemeSetting}_mode`;
+  themeButtonSpan.textContent = `${currentThemeSetting}_mode`;
 
   if (currentThemeSetting)
-    document.documentElement.setAttribute('data-theme', currentThemeSetting);
+    document.documentElement.dataset.theme = currentThemeSetting;
 
   themeButton.addEventListener('click', () => {
     const newTheme = currentThemeSetting === 'dark' ? 'light' : 'dark';
 
-    document.documentElement.setAttribute('data-theme', newTheme);
+    document.documentElement.dataset.theme = newTheme;
 
     localStorage.setItem('theme', newTheme);
     currentThemeSetting = newTheme;
 
-    themeButtonSpan.innerText = `${currentThemeSetting}_mode`;
+    themeButtonSpan.textContent = `${currentThemeSetting}_mode`;
   });
 });
