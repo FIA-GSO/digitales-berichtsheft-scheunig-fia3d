143120c6b2bf4aa8666da4cf7e560c5410db66d1
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-11-04 13:00:06 +0100

    add basic config

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
+/** @import { Linter } from 'eslint' */
+
+import config from '@mephisto5558/eslint-config';
+import globals from 'globals';
+
+/**
+ * @type {Linter.Config[]}
+ * This config lists all rules from every plugin it uses. */
+export default [
+  ...config,
+  {
+    ignores: ['min/**']
+  },
+  {
+    name: 'overwrite',
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.es2024,
+        ...globals.browser
+      }
+    },
+    rules: {
+      'max-nested-callbacks': [
+        'warn',
+        5
+      ],
+      'no-return-assign': 'off',
+      '@stylistic/max-len': 'off',
+      'sonarjs/no-nested-functions': [
+        'error',
+        { threshold: 4 } // 3, + 1 for IIFE
+      ]
+    }
+  }
+];
