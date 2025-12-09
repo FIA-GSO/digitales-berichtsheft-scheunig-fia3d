fed97d56ae51285da64bbb08400bddf484a71f54
sofielean <lehne@lemm.de>
2025-12-09 08:24:41 +0100

    make themebutton switcher more dynamic

--- static/assets/js/theme-changer.js
+++ static/assets/js/theme-changer.js
@@ -11,10 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     themeButtonsContainer = document.querySelector('#themeButtons'),
     /** @type {HTMLButtonElement} */ themeButton = themeButtonsContainer.firstElementChild,
     /** @type {NodeListOf<HTMLButtonElement>} */ themeButtons = themeButtonsContainer.querySelectorAll('.themeButton:not(:first-child)'),
-    themeButtonSpan = themeButton.querySelector('span'),
-    prettyPinkButton = themeButtonsContainer.querySelector('.theme-pretty-pink'),
-    blackGoldButton = themeButtonsContainer.querySelector('.theme-black-gold'),
-    orangeBlackButton = themeButtonsContainer.querySelector('.theme-orange-black');
+    themeButtonSpan = themeButton.querySelector('span');
 
   let currentThemeSetting = localStorage.getItem('theme');
 
@@ -28,18 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
   if (currentThemeSetting)
     document.documentElement.dataset.theme = currentThemeSetting;
 
-  themeButton.addEventListener('click', () => {
-    const newTheme = currentThemeSetting === 'dark' ? 'light' : 'dark';
-
-    document.documentElement.dataset.theme = newTheme;
-
-    localStorage.setItem('theme', newTheme);
-    currentThemeSetting = newTheme;
-
-    const notCurrentThemeSetting = currentThemeSetting === 'dark' ? 'light' : 'dark';
-    themeButtonSpan.textContent = `${notCurrentThemeSetting}_mode`;
-  });
-
   themeButtonsContainer.addEventListener('mouseleave', () => {
     for (const button of themeButtons) button.style.visibility = 'hidden';
   });
@@ -47,22 +32,27 @@ document.addEventListener('DOMContentLoaded', () => {
     for (const button of themeButtons) button.style.visibility = 'visible';
   });
 
-  blackGoldButton.addEventListener('click', () => {
-    document.documentElement.dataset.theme = 'black-gold';
-    localStorage.setItem('theme', 'black-gold');
-    currentThemeSetting = 'black-gold';
-    themeButtonSpan.textContent = 'diamond';
+  themeButton.addEventListener('click', () => {
+    const newTheme = currentThemeSetting === 'dark' ? 'light' : 'dark';
+
+    document.documentElement.dataset.theme = newTheme;
+    localStorage.setItem('theme', newTheme);
+    currentThemeSetting = newTheme;
+    themeButtonSpan.textContent = themes.get(newTheme);
   });
-  orangeBlackButton.addEventListener('click', () => {
-    document.documentElement.dataset.theme = 'orange-black';
-    localStorage.setItem('theme', 'orange-black');
-    currentThemeSetting = 'orange-black';
-    themeButtonSpan.textContent = '18_up_rating';
-  });
-  prettyPinkButton.addEventListener('click', () => {
-    document.documentElement.dataset.theme = 'pretty-pink';
-    localStorage.setItem('theme', 'pretty-pink');
-    currentThemeSetting = 'pretty-pink';
-    themeButtonSpan.textContent = 'favorite';
+
+  themeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let classes = button.className.split(' ');
+
+      const selectedTheme = classes.find((c) => c.startsWith('theme-')).replace('theme-', '');
+
+      if (selectedTheme && themes.has(selectedTheme)) {
+        document.documentElement.dataset.theme = selectedTheme;
+        localStorage.setItem('theme', selectedTheme);
+        currentThemeSetting = selectedTheme;
+        themeButtonSpan.textContent = themes.get(selectedTheme);
+      }
+    });
   });
 });
